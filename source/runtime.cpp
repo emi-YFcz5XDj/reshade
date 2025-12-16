ce669e161d19989e65ee9081575fd80820bb5601
Alice H. <alice.hall0451+vcs@gmail.com>
2025-12-16 21:10:49 +0000

    Revert "Remove splash and load spinner"
    This reverts commit cfe7d6f68a51fadf63c25259820f3804090def35.

--- source/runtime.cpp
+++ source/runtime.cpp
@@ -3528,7 +3528,7 @@ void reshade::runtime::reload_effects(bool force_load_all)
 
 #if RESHADE_GUI
 	_preset_is_modified = false;
-	_show_splash = false;
+	_show_splash = true; // Always show splash bar when reloading everything
 	_reload_count++;
 #endif
 	_last_reload_successful = true;
